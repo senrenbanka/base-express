1faae411bbaaa330f6e637f93ce123ff8073c00e
Kamikase <eryrewy@163.com>
2020-11-10 19:49:47 +0800

    update

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-const express = require('express')
-  , app = express()
-  , bodyParser = require('body-parser')
-  , port = process.env.PORT || 8090
+// const express = require('express')
+//   , app = express()
+//   , bodyParser = require('body-parser')
+//   , port = process.env.PORT || 8090
 
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+// app.use(express.static('public'))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: true}))
 
 
-app.get('/tt_monitor', (req, res) => {
-  res.send('')
+// app.get('/tt_monitor', (req, res) => {
+//   res.send('')
+// })
+
+// app.listen(port, () => console.log(`Listening on port ${port}`))
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+new Promise((resolve, reject) => {
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+  
+  const db = client.db(dbName);
+ 
+  client.close();
+  resolve("ok")
+});
+}).then(msg => {
+  console.log(msg)
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
 
