9c4de1778ef831fe2203599530208db2b0b2a869
Kamikase <eryrewy@163.com>
2020-11-10 13:52:19 +0800

    update

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.get('/api', (req, res) => {
-  res.send('api')
+app.get('/', (req, res) => {
+  res.send('')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
