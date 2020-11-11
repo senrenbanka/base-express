43775647749db3954a46113aa2d49c4589168dc5
Kamikase <eryrewy@163.com>
2020-11-11 16:36:06 +0800

    update

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// const express = require('express')
-//   , app = express()
-//   , bodyParser = require('body-parser')
-//   , port = process.env.PORT || 8090
+const express = require('express')
+  , app = express()
+  , bodyParser = require('body-parser')
+  , port = process.env.PORT || 8090
 
 
-// app.use(express.static('public'))
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 
-// app.get('/tt_monitor', (req, res) => {
-//   res.send('')
-// })
+app.get('/tt_monitor', (req, res) => {
+  res.send('')
+})
 
-// app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 
 
@@ -37,5 +37,4 @@ async function a() {
 
 }
 
-a();
 
